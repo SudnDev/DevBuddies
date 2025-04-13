31f6c80a34d9934f59435e34e785c24d11b0ef6d
S. Nikita <nikita.sudnitsyn1@gmail.com>
2025-04-13 17:45:35 +0500

    Google Analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,12 @@ import './index.css';
 import ThemeSwitcher from './components/ThemeSwitcher';
 import { ThemeProvider } from './contexts/ThemeContext';
 import LanguageSwitcher from "./utils/LanguageSwitcher.tsx";
-import { Helmet } from 'react-helmet-async';
 
 const MainPage = lazy(() => import("./pages/MainPage"));
 
 function App() {
   return (
       <Router>
-
-          <Helmet>
-              {/* Google Tag Manager */}
-              <script>
-                  {`
-                      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','GTM-MC3P5FCS');
-                  `}
-              </script>
-          </Helmet>
-
           <ThemeProvider>
               <div className="fixed top-4 right-4 z-50 flex gap-2">
                   <ThemeSwitcher />
